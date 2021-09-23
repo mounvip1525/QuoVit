39bb6fe298948ab73a2180dc12b987664ae0acf8
adarsh0701 <adarshmishra1237@gmail.com>
2021-09-23 08:24:04 +0530

    QuestionBank page added

--- frontend/src/Screens/QuestionBank/EachCourse.js
+++ frontend/src/Screens/QuestionBank/EachCourse.js
@@ -1,11 +1,54 @@
 import React from 'react'
 import MainLayout from '../../Components/Structure/Main'
 import './css/EachCourse.css'
+import EachCourseCard from '../../Components/Cards/EachCourseCard';
+import { useLocation } from 'react-router';
 
 export default function EachCourse() {
+    // const location=useLocation()
+    // const {c_type}=location.state
+
     return (
         <MainLayout>
-            Each Coirse
+            <div className="courseName">Coursename</div>
+            <div className="courseType">UC</div>
+            <div className="exam">CAT 1 </div> 
+            <div className="cat1">   
+                <div className="c1">
+                <EachCourseCard></EachCourseCard>
+                <EachCourseCard></EachCourseCard>
+                </div>
+                <div className="c2">
+                <EachCourseCard></EachCourseCard>
+                </div>
+                <div className="c3">
+                <EachCourseCard></EachCourseCard>
+                </div>
+           
+            </div> 
+            <hr />
+            CAT 2
+            <div className="cat2">   
+                <div className="c1">
+                <EachCourseCard></EachCourseCard>
+                </div>
+                <div className="c2">
+                <EachCourseCard></EachCourseCard>
+                </div>
+                <div className="c3">
+                <EachCourseCard></EachCourseCard>
+                </div>
+            </div>
+            <hr />
+            FAT 
+            <div className="cat2">   
+                <div className="c1">
+                </div>
+                <div className="c2">
+                </div>
+                <div className="c3">
+                </div>
+            </div>
         </MainLayout>
     )
 }
--- frontend/src/Screens/QuestionBank/QuestionBank.js
+++ frontend/src/Screens/QuestionBank/QuestionBank.js
@@ -1,15 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CourseCard from '../../Components/Cards/CourseCard'
 import MainLayout from '../../Components/Structure/Main'
 import './css/QuestionBank.css'
+import srcpng from './img/search.png'
+// import CourseCard from '../../Components/Cards/CourseCard'
 
 export default function QuestionBank() {
+    const courses=[
+        {
+            id:1,
+            c_type:"UC",
+            c_name:"Data visualization"},
+        {
+            id:2,
+            c_type:"UC",
+            c_name:"Data visualization" 
+            
+        },
+        {
+            id:3,
+            c_type:"UC",
+            c_name:"Data visualization" 
+            
+        },
+    ]
     //Adarsh check the folder Ideas Block to understand the mappings if u dont understand message me, I'll explain! 
     return (
             <MainLayout>
-                <Link to="/EachCourse">
-                    <p>hi</p>
-                </Link>
+                <div className="searchBar">
+                    <input type="text" className="searchTerm" placeholder="What subject are you searching for?"/>
+                    <button type="submit" className="searchButton"><img src={srcpng} alt=""/></button>
+                </div>
+                   <div className="courses">
+                       <div className="c-1">
+                        {/* <Link to="/Eachcourse"style={{color: 'inherit', textDecoration: 'inherit'}}> */}
+                        {courses.map(course=>(
+                            <CourseCard course={course} />
+                                         ))}
+                        {/* </Link> */}
+                       </div>
+                       <div className="c-2">
+                       {/* <Link to="/Eachcourse" style={{color: 'inherit', textDecoration: 'inherit'}}> */}
+                      
+                       {courses.map(course=>(
+                            <CourseCard course={course} />
+                                         ))}
+                       {/* </Link> */}
+                           
+                       </div>
+                    </div>
+              
             </MainLayout>     
     )
 }
